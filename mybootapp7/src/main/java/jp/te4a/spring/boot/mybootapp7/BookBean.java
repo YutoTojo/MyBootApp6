bb52283d57e657eca5c20f20074bbb36407c0e6a
YutoTojo <129241496+YutoTojo@users.noreply.github.com>
2023-06-22 14:23:43 +0900

    06/22

---
+++ mybootapp7/src/main/java/jp/te4a/spring/boot/mybootapp7/BookBean.java
@@ -0,0 +1,29 @@
+package jp.te4a.spring.boot.mybootapp7;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class BookBean  {
+	Integer id ;
+	String title;
+	String writter;
+	String publisher;
+	 Integer price;
+	public Integer getId() {
+		return null;
+	}
+    public String getTitle() {
+        return null;
+    }
+    public String getPublisher() {
+        return null;
+    }
+    public String getWritter() {
+        return null;
+    }
+    public String getPrice() {
+        return null;
+    }
+}
